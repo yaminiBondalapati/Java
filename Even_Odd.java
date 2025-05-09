8bcf53806b3cddd815a09a6d5b738f2a0fb28de7
yaminiBondalapati <yaminiashok728@gmail.com>
2025-05-09 23:17:34 +0530

    Add files via upload

---
+++ Even_Odd.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Even_Odd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number:");
+        int num = sc.nextInt();
+        
+        
+        if(num < 1 || num >= 100) {
+            System.out.println("invalid");
+        }
+        else {
+            if(num < 20) {
+                System.out.println("less than 20");
+            
+                if(num % 2 == 0) {
+                    System.out.println("even");
+                }
+                else {
+                    System.out.println("odd");
+                }
+            }
+            else {
+                System.out.println("greater than 20");
+            }
+        }
+
+        sc.close();
+    }
+}
